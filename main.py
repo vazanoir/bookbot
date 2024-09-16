39207af002f6797906f6c9e63ace6505bdbfa7ff
Vazanoir <8283348-vazanoir@users.noreply.gitlab.com>
2024-09-16 10:58:28 +0200

    add function for counting chars

--- main.py
+++ main.py
@@ -2,10 +2,23 @@ def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
         print(word_count(file_contents))
+        print(char_count(file_contents))
 
 
 def word_count(str):
     return len(str.split())
+
+
+def char_count(str):
+    count = {}
+
+    for c in str.lower():
+        if c in count:
+            count[c] = count[c] + 1
+        else:
+            count[c] = 1
+
+    return count
 
 
 main()
