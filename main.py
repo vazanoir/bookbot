78b7cd8a24dea6aaa86de12c47538d3e2c670c36
Vazanoir <8283348-vazanoir@users.noreply.gitlab.com>
2024-09-16 11:21:56 +0200

    better design

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ def main():
     file = "books/frankenstein.txt"
     with open(file) as f:
         file_contents = f.read()
+
         print(f"--- Begin report of {file} ---")
         print(f"{word_count(file_contents)} words found in the document\n")
+
         chars = char_count(file_contents)
         for c in sorted(chars, key=chars.get, reverse=True):
-            print(f"The '{c}' character was found {chars[c]} times")
+            if ord(c) in range(97, 123):
+                print(f"The '{c}' character was found {chars[c]} times")
+
         print("--- End report ---")
 
 
@@ -18,11 +22,10 @@ def char_count(str):
     count = {}
 
     for c in str.lower():
-        if ord(c) in range(97, 123):
-            if c in count:
-                count[c] = count[c] + 1
-            else:
-                count[c] = 1
+        if c in count:
+            count[c] += 1
+        else:
+            count[c] = 1
 
     return count
 
